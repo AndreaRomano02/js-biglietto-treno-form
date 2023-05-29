ebedcd500cb7bc1a9a434e7ed21fd68903b9b4d1
AndreaRomano02 <andy.romano2002@gmail.com>
2023-05-29 18:05:02 +0200

    Add some Style, button cancel and fix somethings

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@ const pageFinalPrice = document.getElementById("final-price");
 const pageKm = document.getElementById("page-km");
 const pageName = document.getElementById("page-username");
 const pageAge = document.getElementById("page-age");
+const pageCarriege = document.getElementById("carriage");
+const pageCpCode = document.getElementById("cp-code");
+const card = document.getElementById("card");
+let tariff = "Tariffa standard";
 
 //# Chiedo all'utente le informazioni con gli input nel DOM
 const inputName = document.getElementById("user-name");
@@ -53,29 +57,66 @@ generateBtn.addEventListener("click", function () {
   if (!valid) {
     alert(errorMessage);
   } else {
+    //# Mostrare la card
+    card.classList.add("show");
+
     //# Calcolo della tariffa base
     let basePriceKm = userKm * priceForKm;
     let finalPrice;
 
     //# Controllo l'età ed applico lo sconto
-    if (inputAge.value === "minorenne") {
+    const age = inputAge.value;
+    if (age === "minorenne") {
       finalPrice = (basePriceKm * discountMinor).toFixed(2);
-    } else if (inputAge.value === "over 65") {
+      tariff = "Tariffa ridotta";
+    } else if (age === "over 65") {
       finalPrice = (basePriceKm * discountOver).toFixed(2);
+      tariff = "Tariffa over";
     }
+
+    //# Random
+    //? Variabili random
+    const random = Math.random();
+    const minCode = 100000;
+    const maxCode = 150000;
+    const maxCarriage = 15;
+
+    //? Formula Carrozza
+    let resultCarriege = Math.floor(random * maxCarriage + 1);
+    console.log(resultCarriege);
+
+    //? Formula Cp codice
+    let resultCpCode = Math.floor(random * (maxCode + 1 - minCode)) + minCode;
+    console.log(resultCpCode);
 
     //# Arrotondo a due decimali massimi
     basePriceKm = basePriceKm.toFixed(2);
 
     //# Stampo in pagina
     pageName.innerText = userName;
+
     pageKm.innerText = userKm + "Km";
-    pageAge.innerText = inputAge.value;
+
+    pageAge.innerText = tariff;
+
     pageBasePrice.innerText = basePriceKm;
     if (!finalPrice) {
       pageFinalPrice.innerText = basePriceKm;
     } else {
       pageFinalPrice.innerText = finalPrice;
     }
+
+    pageCarriege.innerText = resultCarriege;
+    pageCpCode.innerText = resultCpCode;
   }
+});
+
+cancelBtn.addEventListener("click", function () {
+  //# Svuotare gli input
+  inputAge.value = "";
+  inputKm.value = "";
+  inputName.value = "";
+
+  //#Cancellare il biglietto
+  card.classList.remove("show");
 });
